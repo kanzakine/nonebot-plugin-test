c2f8795452ae8a306ff8f69a22e71c162fdedc35
kanzakiD <1690800500@qq.com>
2025-02-10 09:34:45 +0800

    normal run test

--- nonebot_plugin_test/__init__.py
+++ nonebot_plugin_test/__init__.py
@@ -1,22 +1,18 @@
 from nonebot.plugin import PluginMetadata
-
 from .config import Config
+from nonebot import on_command
 
 __plugin_meta__ = PluginMetadata(
     name="test",
     description="测试用",
     usage="test",
-
     type="application",
-    # 发布必填，当前有效类型有：`library`（为其他插件编写提供功能），`application`（向机器人用户提供功能）。
-
     homepage="https://github.com/kanzakine/nonebot-plugin-test",
-    # 发布必填。
-
     config=Config,
-    # 插件配置项类，如无需配置可不填写。
-
     supported_adapters={"~onebot.v11", "~telegram"},
-    # 支持的适配器集合，其中 `~` 在此处代表前缀 `nonebot.adapters.`，其余适配器亦按此格式填写。
-    # 若插件可以保证兼容所有适配器（即仅使用基本适配器功能）可不填写，否则应该列出插件支持的适配器。
 )
+
+weather = on_keyword("test")
+@weather.handle()
+async def handle_function():
+    await weather.finish("message")
